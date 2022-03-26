ba0aface5b0ea2be41f944a681024306719b0920
Redson <redson@riseup.net>
2022-03-26 09:19:34 -0300

    Now it's working fine.

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-var sorteio = Math.floor(Math.random() * 2)
+let sorteio = Math.floor(Math.random() * 3)
 
 function getInputValue() {
   let val0 = document.getElementById("InputOne").value
   let val1 = document.getElementById("InputTwo").value
-
-    if(sorteio < 1) {
+	if(!val0 || !val1) {
+		console.log("Tem que inserir algo né serjão!")
+		return 1;
+	}
+    if(`${Math.floor(Math.random() * 2)}` == 0) {
+    document.getElementById("konqidiz").innerHTML = "Konqi escolheu: ";
+    document.getElementById("resultado").innerHTML = val0;
+	} else {
     document.getElementById("konqidiz").innerHTML = "Konqi escolheu: ";
     document.getElementById("resultado").innerHTML = val1;
-  } else {}
-    console.log(`Valor 1: ${val0}\n Valor2: ${val1}\n Aleatorio: ${sorteio}`)
+	}
+    console.log(`Valor 1: ${val0}\nValor 2: ${val1}\nAleatorio: ${sorteio}`)
 }
