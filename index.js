9d3b7093cb5a0674718e896612f195a23deb674a
Redson <redson@riseup.net>
2022-06-13 18:55:00 -0300

    fix: innerText instead of innerHTML

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ function getInputValue() {
 		return 1;
 	}
     if(`${Math.floor(Math.random() * 2)}` == 0) {
-    document.getElementById("konqidiz").innerHTML = "Konqi escolheu: ";
-    document.getElementById("resultado").innerHTML = val0;
+    document.getElementById("konqidiz").innerText = "Konqi escolheu: ";
+    document.getElementById("resultado").innerText = val0;
 	} else {
-    document.getElementById("konqidiz").innerHTML = "Konqi escolheu: ";
-    document.getElementById("resultado").innerHTML = val1;
+    document.getElementById("konqidiz").innerText = "Konqi escolheu: ";
+    document.getElementById("resultado").innerText = val1;
 	}
     console.log(`Valor 1: ${val0}\nValor 2: ${val1}\nAleatorio: ${sorteio}`)
 }
