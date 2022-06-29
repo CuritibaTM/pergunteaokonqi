2a7e25cbd2c4d647afdd67d0e5a057a3b707adf1
leo78913 <69596237+leo78913@users.noreply.github.com>
2022-06-29 13:46:07 -0300

    :nerd:

--- index.js
+++ index.js
@@ -1,18 +1,9 @@
-let sorteio = Math.floor(Math.random() * 3)
-
 function getInputValue() {
-  let val0 = document.getElementById("InputOne").value
-  let val1 = document.getElementById("InputTwo").value
+    let val0 = document.getElementById("InputOne").value
+    let val1 = document.getElementById("InputTwo").value
 	if(!val0 || !val1) {
-		console.log("Tem que inserir algo né serjão!")
 		return 1;
 	}
-    if(`${Math.floor(Math.random() * 2)}` == 0) {
     document.getElementById("konqidiz").innerText = "Konqi escolheu: ";
-    document.getElementById("resultado").innerText = val0;
-	} else {
-    document.getElementById("konqidiz").innerText = "Konqi escolheu: ";
-    document.getElementById("resultado").innerText = val1;
-	}
-    console.log(`Valor 1: ${val0}\nValor 2: ${val1}\nAleatorio: ${sorteio}`)
+    document.getElementById("resultado").innerText = Math.random() < .5 ? val0 : val1;
 }
